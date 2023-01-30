5bc17da58e7ed12bfb84602f3cc763d0d9617c32
GuilhermeCasagrandeF <guilherme.casagrande@soeltech.com.br>
2023-01-30 15:34:30 -0300

    Pegar os dados do form com two-way data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { AppNovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppNovaTransferenciaComponent } from './nova-transferencia/nova-transfe
     AppNovaTransferenciaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,11 +1,17 @@
 import { Component } from "@angular/core";
 
 @Component({
-    selector: 'app-nova-transferencia',
-    templateUrl: './nova-transferencia.component.html',
-    styleUrls: ['./nova-transferencia-component.scss']
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia-component.scss']
 })
 
 export class AppNovaTransferenciaComponent {
 
+  valor: number;
+  destino: number;
+
+  transferir() {
+    console.log(this.valor, this.destino);
+  }
 }
