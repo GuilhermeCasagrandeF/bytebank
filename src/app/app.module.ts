fd144ae5af9fda3c3527152930dda509470f3c6e
GuilhermeCasagrandeF <118361478+GuilhermeCasagrandeF@users.noreply.github.com>
2023-02-08 19:59:40 -0300

    Componente de transferencia criando propriedade para binding (property binding)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppNovaTransferenciaComponent } from './nova-transferencia/nova-transferencia.component';
 import { FormsModule } from '@angular/forms';
+import { ExtratoComponent } from './extrato/extrato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppNovaTransferenciaComponent
+    AppNovaTransferenciaComponent,
+    ExtratoComponent
   ],
   imports: [
     BrowserModule,
